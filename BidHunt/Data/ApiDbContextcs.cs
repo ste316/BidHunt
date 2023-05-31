895f3e659fad3b80f1a8e713e4486b08eee51fb2
ste316 <steprato003@gmail.com>
2023-05-31 17:07:42 +0200

    update

---
+++ BidHunt/Data/ApiDbContextcs.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BidHunt.Data
+{
+    public class ApiDbContextcs: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=
+                (localdb)\MSSQLLocalDB;
+                Database=DbHunt");
+        }
+    }
+}
